7131e8ef1544a91edc7c7ffea6a69fb17d1f12dd
MStuv <markstuver@MacBook-Pro.local>
2013-12-01 21:42:44 -0500

    7.20 - 7.21 Changed to iPad App & setup storyboard

--- Race to the Top/RTSpaceShipView.m
+++ Race to the Top/RTSpaceShipView.m
@@ -31,6 +31,8 @@
     bezierPath.lineWidth = 2.0;
     
     [bezierPath moveToPoint:CGPointMake(1/6.0 * self.bounds.size.width, 1/3.0 * self.bounds.size.height)];
+    NSLog(@"%f",1/3.0 * self.bounds.size.height);
+    
     
     [bezierPath addLineToPoint:CGPointMake(1/6.0 * self.bounds.size.width, 2/3.0 * self.bounds.size.height)];
     
