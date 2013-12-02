2a29dcdaa5a100354476bf6b558c6041e5fe8fde
MStuv <markstuver@MacBook-Pro.local>
2013-12-01 23:22:44 -0500

    7.22 - 7.24 Finishing Mountain Path

--- Race to the Top/RTMountainPath.m
+++ Race to the Top/RTMountainPath.m
@@ -8,33 +8,110 @@
 
 #import "RTMountainPath.h"
 
+#define PATH_WIDTH 60
+
 @implementation RTMountainPath
 
-/// Class Method will return an array of bezierPaths
-+(NSArray *)mountainPathsForRect: (CGRect)rect
+
+/// Class method that returns an Array
++ (NSArray *)mountainPathsForRect:(CGRect)rect
+
 {
-    /// Mutable Array instance
     NSMutableArray *variousPaths = [@[] mutableCopy];
     
-    /// Instance of CGPoint
+    /// CGPoints for First Path
     CGPoint firstPoint = CGPointMake(rect.size.width * (1/6.0), CGRectGetMaxY(rect));
     
-    /// Instance of CGPoint
     CGPoint secondPoint = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (5/6.0));
     
-    /// Create instance of UIBezierPath
-    UIBezierPath *labyrinthPath = [UIBezierPath bezierPath];
-    /// Set line width
-    labyrinthPath.lineWidth = 4.0;
-    /// Set moveToPoint to the value of firstPoint
-    [labyrinthPath moveToPoint:firstPoint];
-    /// add line to point to the value of secondPoint
-    [labyrinthPath addLineToPoint:secondPoint];
-    /// add the UIBezierPath instance to the array.
-    [variousPaths addObject:labyrinthPath];
-    /// return array
-    return variousPaths;
+    CGPoint thirdPoint = CGPointMake(CGRectGetMaxX(rect), rect.size.height * (5/6.0));
     
+    CGPoint fourthPoint = CGPointMake(CGRectGetMaxX(rect), rect.size.height * (2/3.0));
+    
+    CGPoint fifthPoint = CGPointMake(rect.size.width * (1/6.0), rect.size.height * (6/12.0));
+    
+    CGPoint sixthPoint = CGPointMake(rect.size.width * (1/6.0), rect.size.height * (6/12.0));
+    
+    CGPoint seventhPoint = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (2/6.0));
+    
+    CGPoint eigthPoint = CGPointMake(rect.size.width * (2/3.0), rect.size.height * (6/12.0));
+    
+    CGPoint ninthPoint = CGPointMake(rect.size.width * (11/20.0), rect.size.height * (5/24.0));
+    
+    /// control point is used when adding a curved path
+    CGPoint controlPoint = CGPointMake(rect.size.width * (6 / 8.0), rect.size.height * (1/ 3.0));
+    
+    
+    /// CGPoints for Second Path
+    CGPoint firstPointForSecondPath = CGPointMake(rect.size.width * (1/6.0) - PATH_WIDTH, CGRectGetMaxY(rect));
+    
+    CGPoint secondPointForSecondPath = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (5/6.0) - PATH_WIDTH);
+    
+    CGPoint thirdPointForSecondPath = CGPointMake(CGRectGetMaxX(rect) - PATH_WIDTH, rect.size.height * (5/6.0) - PATH_WIDTH);
+    
+    CGPoint fourthPointForSecondPath = CGPointMake(CGRectGetMaxX(rect)- PATH_WIDTH, rect.size.height * (2/3.0) + PATH_WIDTH);
+    
+    CGPoint fifthPointForSecondPath = CGPointMake(rect.size.width * (1/6.0) - PATH_WIDTH, rect.size.height * (6/12.0) + PATH_WIDTH / 2);
+    
+    CGPoint sixthPointForSecondPath = CGPointMake(rect.size.width * (1/6.0) - PATH_WIDTH, rect.size.height * (6/12.0));
+    
+    CGPoint seventhPointForSecondPath = CGPointMake(rect.size.width * (1/3.0) - PATH_WIDTH / 4, rect.size.height * (2/6.0) - PATH_WIDTH);
+    
+    CGPoint eigthPointForSecondPath = CGPointMake(rect.size.width * (2/3.0) - PATH_WIDTH, rect.size.height * (6/12.0) - PATH_WIDTH);
+    
+    CGPoint ninthPointForSecondPath = CGPointMake(rect.size.width * (11/20.0) - PATH_WIDTH, rect.size.height * (5/24.0));
+    
+    CGPoint controlPointForSecondpath = CGPointMake(rect.size.width * (6 / 8.0) - PATH_WIDTH, rect.size.height * (1/ 3.0));
+    
+    UIBezierPath *rightMountainPath = [UIBezierPath bezierPath]; // alternative to alloc, init, nested method calls
+    
+    UIBezierPath *leftMountainPath = [UIBezierPath bezierPath];
+    
+    /// Set lineWidth for UIBezierPaths
+    rightMountainPath.lineWidth = 4.0;
+    leftMountainPath.lineWidth = 4.0;
+    
+    [rightMountainPath moveToPoint:firstPoint];
+    
+    [rightMountainPath addLineToPoint:secondPoint];
+    
+    [rightMountainPath addLineToPoint:thirdPoint];
+    
+    [rightMountainPath addLineToPoint:fourthPoint];
+    
+    [rightMountainPath addLineToPoint:fifthPoint];
+    
+    [rightMountainPath addLineToPoint:sixthPoint];
+    
+    [rightMountainPath addLineToPoint:seventhPoint];
+    
+    [rightMountainPath addLineToPoint:eigthPoint];
+    
+    /// addQuadCurveToPoint uses a controlPoint to figure out how great the curve is relative to the control point.
+    [rightMountainPath addQuadCurveToPoint:ninthPoint controlPoint:controlPoint];
+    
+    [leftMountainPath moveToPoint:firstPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:secondPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:thirdPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:fourthPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:fifthPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:sixthPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:seventhPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:eigthPointForSecondPath];
+    
+    [leftMountainPath addQuadCurveToPoint:ninthPointForSecondPath controlPoint:controlPointForSecondpath];
+    
+    /// Added UIBezierPath objects to the arrays
+    [variousPaths addObject:rightMountainPath];
+    [variousPaths addObject:leftMountainPath];
+    
+    return [variousPaths copy];
 }
-
 @end
--- Race to the Top/RTPathView.m
+++ Race to the Top/RTPathView.m
@@ -11,13 +11,34 @@
 
 @implementation RTPathView
 
+
+/// This init is not used when using the storyboard, but in case there is a situation where the storyboard is not used, the initWithFrame is coded to initilize using the 'setup' method.
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        /// call setup method
+        [self setup];
     }
     return self;
+}
+
+/// initWithFrame: is not called when using a storyboard. Instead we actually 'unarchiving' or 'unserializing' it from our storyboard. So to assure that the background is set to clear, we call the method in initWithCoder.
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        /// call setup method
+        [self setup];
+    }
+    return self;
+}
+
+/// Method to set the background color of the view to clear
+-(void)setup
+{
+    self.backgroundColor = [UIColor clearColor];
 }
 
 // Only override drawRect: if you perform custom drawing.
